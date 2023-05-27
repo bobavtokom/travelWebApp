e3f0f24597a60e8e84c6dad6d022567dcfaab8e7
Boban Milosevik <bobavtokom888@gmail.com>
2023-05-27 20:49:00 +0200

    extension names changed on pages, routes added

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => console.log("server started"));
+
+app.get("/", function(req, res){
+    res.render("pages/index");
+});
+app.get("/home", function(req,res){
+    res.render('pages/htmlApp');
+});
+app.get("/about", function(req,res){
+    res.render('pages/about');
+});
+app.get("/contact", function(req,res){
+    res.render('pages/contact');
+});
