fb2f1986ca6e2588f65a3583df62dc8b89c46cd9
Boban Milosevik <bobavtokom888@gmail.com>
2023-05-28 23:45:05 +0200

    mongo and mongoose attempt made

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const ejs = require("ejs");
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+var db;
+const mongoose = require('mongoose');
 
-app.listen(process.env.PORT || 3000, () => console.log("server started"));
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/firstdb');
+};
+
+app.listen(process.env.PORT || 3000, () => console.log("connected"));
 
 app.get("/", function(req, res){
     res.render("pages/index");
