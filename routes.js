d0f70d24425e062cc7eb1a82cd39d60e9e8a062d
Boban Milosevik <bobavtokom888@gmail.com>
2023-06-11 10:12:37 +0200

    conflict with destinationModel.js fixed

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const DestinationModel = require('./models/DestinationModel');
+const DestinationModel = require('./models/destinationModel');
 const ChatMessage = require('./models/LiveChatModel');
 
 
